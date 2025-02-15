3e34338ccb9946aace5021e812f1c62b1f11c19e
mihai271828 <pruneanumihai@gmail.com>
2025-02-15 15:30:13 +0200

    methods in Kiste implementiert

--- kiste/Kiste.java
+++ kiste/Kiste.java
@@ -1,4 +1,31 @@
 package kiste;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Kiste {
+    private List<Sache> sachen=new ArrayList<>();
+    public void add(Sache s) {
+        sachen.add(s);
+    };
+    public List<Sache> schneidenSache(){
+        List<Sache> things=new ArrayList<>();
+        for(Sache s:sachen){
+            if(s.canCut())
+            {
+                things.add(s);
+            }
+        }
+        return things;
+    }
+    public List<Sache> heavySachen(){
+        List<Sache> things=new ArrayList<>();
+        for(Sache s:sachen){
+            if(s.getWeight()>1){
+                things.add(s);
+            }
+        }
+        return things;
+    }
+
 }
--- kiste/Naegel.java
+++ kiste/Naegel.java
@@ -1,22 +1,12 @@
 package kiste;
 
 public class Naegel implements Sache{
-    public double weight;
-
-    public Naegel(double weight) {
-        this.weight = weight;
-    }
     public double getWeight() {
-        return weight;
+        return 0.5;
     }
     public boolean canCut(){
         return true;
     }
 
-    @Override
-    public String toString() {
-        return "Naegel{" +
-                "weight=" + weight +
-                '}';
-    }
+
 }
